1a6f22714968a4eb62b9e3359eeb3833aafef206
Tao Yang <tao.yang@zalando.de>
2016-10-12 15:44:31 +0200

    #1 rename some identifiers

--- core/src/main/scala/org/zalando/conf4s/FromConfig.scala
+++ core/src/main/scala/org/zalando/conf4s/FromConfig.scala
@@ -11,29 +11,29 @@ object FromConfig {
 
   def create[A, B](a: A)(implicit fc: FromConfig[A, B]): B = fc.apply(a)
 
-  implicit def aFromConfigHNil[A]: FromConfig[A, HNil] = new FromConfig[A, HNil] {
+  implicit def hnilFromConfigA[A]: FromConfig[A, HNil] = new FromConfig[A, HNil] {
     def apply(a: A): HNil =  HNil
   }
 
   implicit def aFromConfigHCons[A, H, T <: HList](implicit
-    aFromConfigH: FromConfig[A, H],
-    aFromConfigT: FromConfig[A, T]
+    hFromConfigA: FromConfig[A, H],
+    tFromConfigA: FromConfig[A, T]
   ): FromConfig[A, H :: T] = new FromConfig[A, H :: T] {
 
     def apply(a: A): H :: T = {
-      val h = aFromConfigH(a)
-      val t = aFromConfigT(a)
+      val h = hFromConfigA(a)
+      val t = tFromConfigA(a)
       h :: t
     }
   }
 
   implicit def genericFromConfig[A, B, ReprB](implicit
     gen: Generic.Aux[B, ReprB],
-    aFromConfigReprB: FromConfig[A, ReprB]
+    reprbFromConfigA: FromConfig[A, ReprB]
   ): FromConfig[A, B] = new FromConfig[A, B] {
 
     def apply(a: A): B = {
-      val internal = aFromConfigReprB(a)
+      val internal = reprbFromConfigA(a)
       gen.from(internal)
     }
   }
