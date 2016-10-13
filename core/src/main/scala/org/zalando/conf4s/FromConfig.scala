0e1be96b5252afff48c49055772fd9651ae844e5
Tao Yang <tao.yang@zalando.de>
2016-10-13 11:36:04 +0200

    #1 uses labelled generics

--- core/src/main/scala/org/zalando/conf4s/FromConfig.scala
+++ core/src/main/scala/org/zalando/conf4s/FromConfig.scala
@@ -1,6 +1,7 @@
 package org.zalando.conf4s
 
-import shapeless._
+import shapeless.{::, HNil, HList, Witness, LabelledGeneric}
+import shapeless.labelled.{FieldType, field}
 
 trait FromConfig[A, B] {
 
@@ -15,20 +16,21 @@ object FromConfig {
     def apply(a: A): HNil =  HNil
   }
 
-  implicit def aFromConfigHCons[A, H, T <: HList](implicit
+  implicit def aFromConfigHCons[A, K <: Symbol, H, T <: HList](implicit
+    key: Witness.Aux[K],
     hFromConfigA: FromConfig[A, H],
     tFromConfigA: FromConfig[A, T]
-  ): FromConfig[A, H :: T] = new FromConfig[A, H :: T] {
+  ): FromConfig[A, FieldType[K, H] :: T] = new FromConfig[A, FieldType[K, H] :: T] {
 
-    def apply(a: A): H :: T = {
+    def apply(a: A): FieldType[K,H] :: T = {
       val h = hFromConfigA(a)
       val t = tFromConfigA(a)
-      h :: t
+      field[K](h) :: t
     }
   }
 
   implicit def genericFromConfig[A, B, ReprB](implicit
-    gen: Generic.Aux[B, ReprB],
+    gen: LabelledGeneric.Aux[B, ReprB],
     reprbFromConfigA: FromConfig[A, ReprB]
   ): FromConfig[A, B] = new FromConfig[A, B] {
 
