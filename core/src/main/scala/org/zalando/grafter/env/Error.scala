a67875c2182a847692631555b0441d6cc812e418
Tao Yang <tao.yang@zalando.de>
2016-10-14 18:04:09 +0200

    #1 removes env package for 1st task

--- core/src/main/scala/org/zalando/grafter/env/Error.scala
+++
@@ -1,6 +0,0 @@
-package org.zalando.grafter.env
-
-sealed trait EnvError
-final case class EnvNotFound(name: String) extends EnvError
-final case class EnvParseError(name: String, raw: String) extends EnvError
-final case class FromStrError(message: String) extends EnvError
