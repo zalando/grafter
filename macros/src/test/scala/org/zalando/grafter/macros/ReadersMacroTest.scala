a1b93cc708f2b5b9223fbd9878c8d87bae8cd2cb
Eric Torreborre <etorreborre@yahoo.com>
2017-06-14 08:22:11 +0200

    #80 added an identity reader for the @readers annotation

--- macros/src/test/scala/org/zalando/grafter/macros/ReadersMacroTest.scala
+++ macros/src/test/scala/org/zalando/grafter/macros/ReadersMacroTest.scala
@@ -1,14 +1,29 @@
 package org.zalando.grafter.macros
 
-object ReadersMacroTest {
-  val r1: cats.data.Reader[AppConfig, C1] =
-    AppConfig.c1Reader
+import cats.data.Reader
 
-  val r2: cats.data.Reader[AppConfig, C2] =
+object ReadersMacroTest {
+  val r1: Reader[AppConfig, C1] =
+    AppConfig.c1Reader
+  
+  val r1Implicit: Reader[AppConfig, C1] =
+    implicitly[Reader[AppConfig, C1]]
+
+  val r2: Reader[AppConfig, C2] =
     AppConfig.c21Reader
 
+  val r2Implicit: Reader[AppConfig, C2] =
+    implicitly[Reader[AppConfig, C2]]
+
+  val r: Reader[AppConfig, AppConfig] =
+   AppConfig.appConfigReader
+
+  val rImplicit: Reader[AppConfig, AppConfig] =
+    implicitly[Reader[AppConfig, AppConfig]]
+
+
 // this doesn't compile because only one Reader instance of a given type can be generated
-//  val r3: cats.data.Reader[AppConfig, C2] =
+//  val r3: Reader[AppConfig, C2] =
 //    AppConfig.c22Reader
 }
 trait T1
