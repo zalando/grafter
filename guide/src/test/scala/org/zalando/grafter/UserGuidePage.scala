efcb4abbccd222334c70a3a82eabeaa0661a1828
Eric Torreborre <etorreborre@yahoo.com>
2017-11-10 15:18:24 +0100

    fixup! fixup! fixup! finalized and published the user guide

--- guide/src/test/scala/org/zalando/grafter/UserGuidePage.scala
+++ guide/src/test/scala/org/zalando/grafter/UserGuidePage.scala
@@ -1,9 +1,7 @@
 package org.zalando.grafter
 
 import org.specs2.Specification
-import org.specs2.main.Arguments
 import org.specs2.specification.Snippets
-import org.specs2.specification.core.SpecStructure
 
 import scala.io.Source
 
@@ -12,10 +10,7 @@ import scala.io.Source
  */
 abstract class UserGuidePage extends Specification with Snippets {
 
-  override def map(structure: SpecStructure) =
-    super.map(structure.setArguments(Arguments("html.nostats")).map(_.compact))
-
-
   def version: String =
     Source.fromFile("../version.sbt").getLines.toList.head.trim.split(" ").last.replace("\"", "")
+
 }
